848632ad08b3b8454de9ddc10e9a686eed74b94c
Javier Farre <jfarre90@gmail.com>
2019-03-15 12:45:44 +0000

    other solution implemented

--- src/App.js
+++ src/App.js
@@ -26,13 +26,15 @@ class App extends Component {
       const randomInstructorIndex = Math.floor(Math.random()*this.state.instructors.length);
       const randomHobbyIndex = Math.floor(Math.random()*this.state.instructors[randomInstructorIndex]);
       
-      const instructors = this.state.instructors.slice();
-      //we could be tempted to now use instructors[randomInstructorIndex].hobbies.splice(hobbyIndex, 1), because we have made the instructors copy, but this AFFECTS the original state due to the splice method. Never use splice when changing states (reference is the SAME object).
-      instructors[randomInstructorIndex] = Object.assign({}, instructors[randomInstructorIndex]); //with this we create another reference so we can mess with it
-      
-      instructors[randomInstructorIndex].hobbies = instructors[randomInstructorIndex].hobbies.slice(); //the hobbies is still using the original reference, hence why we need to create a copy
-      instructors[randomInstructorIndex].hobbies.splice(randomHobbyIndex, 1);
-      this.setState({instructors});
+      const instructors = this.state.instructors.map((inst,i) => (
+        i === randomInstructorIndex ? {
+          ...inst,
+          hobbies: [...inst.hobbies.slice(0, randomHobbyIndex).concat(inst.hobbies.slice(randomHobbyIndex+1, inst.hobbies.length))]
+          
+        } : inst
+        
+      ));
+        
     },5000) // Using arrow function to preserve the "this" definition.
   }
   render() {
